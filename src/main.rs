d72d1609e0628d08438501584a2621fb2cab6889
bradleygichuru <prz03@protonmail.com>
2023-07-02 02:22:27 +0300

    adds list traversal,deletion

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,23 @@ use std::ptr::NonNull;
 struct Node<T> {
     data: T,
     next: Option<NonNull<Node<T>>>,
+    is_head: bool,
 }
+#[derive(Debug)]
 struct SinglyLinkedList<T> {
     head: Option<NonNull<Node<T>>>,
 }
-impl<T> SinglyLinkedList<T> {
-     fn new()->SinglyLinkedList<T>{
-        SinglyLinkedList { head: None}
-
-
-    } 
+impl<T: std::cmp::PartialEq + std::fmt::Debug> SinglyLinkedList<T> {
+    fn new() -> SinglyLinkedList<T> {
+        SinglyLinkedList { head: None }
+    }
     fn insert_beginning(&mut self, data: T) -> bool {
         //insert a node at the beginning of the list
         if self.head.is_none() {
             let new_node = Box::new(Node {
                 data: data,
                 next: None,
+                is_head: true,
             });
 
             let head_ptr = Box::leak(new_node).into();
@@ -28,6 +29,7 @@ impl<T> SinglyLinkedList<T> {
             let new_node = Box::new(Node {
                 data: data,
                 next: self.head,
+                is_head: true,
             });
 
             let head_ptr = Box::leak(new_node).into();
@@ -35,15 +37,16 @@ impl<T> SinglyLinkedList<T> {
             true
         }
     }
-    fn insert_end(self, data: T) -> bool {
+    fn insert_end(&mut self, data: T) -> bool {
         //insert a node at the end of the list
         // let current_node = &self.head;
         if self.head.is_some() {
             let mut current_node = Box::new(self.head);
-            
+
             let new_node = Box::new(Node {
                 data: data,
                 next: None,
+                is_head: false,
             });
             let new_node_pointer = Box::leak(new_node).into();
             let mut current_node_ptr: &mut Option<NonNull<Node<T>>> =
@@ -58,7 +61,94 @@ impl<T> SinglyLinkedList<T> {
                 true
             }
         } else {
+            println!("Linked list has no head");
             false
+        }
+    }
+    fn delete_node(&mut self, data: T) {
+        if self.head.is_some() {
+            let mut current_node = Box::new(self.head);
+            let mut prev_node = Box::new(self.head);
+            let mut prev_node_ptr: &mut Option<NonNull<Node<T>>> = Box::leak(prev_node).into();
+            let mut current_node_ptr: &mut Option<NonNull<Node<T>>> =
+                Box::leak(current_node).into();
+            unsafe {
+                while current_node_ptr.unwrap().as_mut().next.is_some() {
+                    if current_node_ptr.unwrap().as_mut().data == data {
+                        //if current node is our target node
+                        if prev_node_ptr.unwrap().as_mut().is_head == false {
+                            //if not the head node , unlink the current node by setting the previous
+                            //node's next pointer to the current node's next pointer
+                            //pointer to the previous's node next node
+                            prev_node_ptr.unwrap().as_mut().next =
+                                current_node_ptr.unwrap().as_mut().next;
+                            break;
+                        } else {
+                            current_node_ptr
+                                .unwrap()
+                                .as_mut()
+                                .next
+                                .unwrap()
+                                .as_mut()
+                                .is_head = true;
+                            self.head = *current_node_ptr;
+                        }
+                    } else {
+                        prev_node = Box::new(*current_node_ptr);
+                        prev_node_ptr = Box::leak(prev_node);
+                        current_node = Box::new(current_node_ptr.unwrap().as_mut().next);
+
+                        current_node_ptr = Box::leak(current_node).into();
+                    }
+                }
+            }
+        } else {
+            println!("Linked list has no head");
+        }
+    }
+    // fn search(data: T) -> {}
+    fn traverse(&mut self) {
+        if self.head.is_some() {
+            let mut current_node = Box::new(self.head);
+
+            let mut current_node_ptr: &mut Option<NonNull<Node<T>>> =
+                Box::leak(current_node).into();
+            unsafe {
+                /* while current_node_ptr.unwrap().as_mut().next.is_some() {
+                    println!("data : {:?}",current_node_ptr.unwrap().as_mut().data);
+                    current_node = Box::new(current_node_ptr.unwrap().as_mut().next);
+
+                    current_node_ptr = Box::leak(current_node).into();
+                } */
+                loop {
+                    /* println!(
+                        "data : {:?} ,next: {:?}",
+                        current_node_ptr.unwrap().as_mut().data,
+                        current_node_ptr
+                            .unwrap()
+                            .as_mut()
+                            .next
+                            .unwrap()
+                            .as_mut()
+                            .data
+                    ); */
+                    if current_node_ptr.unwrap().as_mut().next.is_none() {
+                        println!("data: {:?}", current_node_ptr.unwrap().as_mut().data);
+
+                        break;
+                    }
+
+                    if current_node_ptr.unwrap().as_mut().next.is_some() {
+                        println!("data: {:?}", current_node_ptr.unwrap().as_mut().data);
+
+                        current_node = Box::new(current_node_ptr.unwrap().as_mut().next);
+
+                        current_node_ptr = Box::leak(current_node).into();
+                    }
+                }
+            }
+        } else {
+            println!("Linked list has no head");
         }
     }
 }
@@ -66,13 +156,24 @@ fn main() {
     println!("Hello, world!");
 }
 #[test]
-fn test_insert(){
+fn test_insert() {
     let mut ln = SinglyLinkedList::new();
+
     ln.insert_beginning(1);
     ln.insert_beginning(2);
-    unsafe{
-        assert_eq!(ln.head.unwrap().as_mut().data,2);
-    println!("head {:?}",ln.head.unwrap().as_mut().data);
 
+    ln.insert_beginning(3);
+
+    ln.insert_beginning(4);
+    ln.insert_beginning(5);
+    ln.insert_beginning(6);
+    ln.insert_beginning(7);
+    ln.insert_beginning(8);
+    ln.traverse();
+    //ln.delete_node(1);
+
+    unsafe {
+        assert_eq!(ln.head.unwrap().as_mut().data, 8);
+        println!("head : {:?}", ln.head.unwrap().as_mut().data);
     }
 }
